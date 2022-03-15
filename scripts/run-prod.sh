12512ca8cbedffe959b8dee093647162a409314e
Nick Novikov <nick.novikov@gmail.com>
2022-03-15 16:05:09 +0800

    scripts: Deploy to the server

---
+++ scripts/run-prod.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+set -e
+
+
+cd "$(dirname "$0")/../sumbur"
+
+CGO_ENABLED=0  go build  -o ../public/sumbur  -v
+
+strip -s ../public/sumbur
+
+
+cd ..
+
+rsync \
+    --archive \
+    --delete-excluded \
+    --exclude "sumbur-debug*" \
+    --verbose \
+    public/ \
+    sumbur.info:/srv/web/sumbur.info/sumbur/
